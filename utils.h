688a10cf8a1964c30c4c8db976a888bcdf70817b
RecepSen <recep.sen@ayvos.com>
2018-02-20 13:45:26 +0300

    Newest

--- utils.h
+++ utils.h
@@ -5,7 +5,8 @@
 #include <cuda.h>
 #include <iostream>
 #include <cstring>
-
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 inline cudaError_t checkCuda(cudaError_t result)
 {
@@ -18,6 +19,24 @@ inline cudaError_t checkCuda(cudaError_t result)
 
 inline void codeFlowControl(std::string s){
 	std::cout << s << std::endl;
+}
+
+inline void splitImage2Channels(cv::Mat img, uchar *r_ch, uchar *g_ch, uchar *b_ch){
+    for(int row = 0; row < img.rows; row++){
+        for(int col = 0; col < img.cols; col++){
+            r_ch[row * img.cols + col] = img.at<cv::Vec3b>(row, col)[2];
+            g_ch[row * img.cols + col] = img.at<cv::Vec3b>(row, col)[1];
+            b_ch[row * img.cols + col] = img.at<cv::Vec3b>(row, col)[0];
+        }
+    }
+}
+
+inline void vector2Image(uchar *res, cv::Mat &img, int rows, int cols){
+    for(int row = 0; row < rows; row++){
+        for(int col = 0; col < cols; col++){
+            img.at<uchar>(row,col) = res[row * cols + col];
+        }
+    }
 }
 
 #endif
