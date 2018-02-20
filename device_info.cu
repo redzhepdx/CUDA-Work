480be36a69d3c6e4d2b389ddb323c7ad878b90bc
redzhepdx <redzhepdx@users.noreply.github.com>
2018-02-20 15:55:48 +0300

    Create device_info.cu

---
+++ device_info.cu
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int deviceId;
+  
+  cudaGetDevice(&deviceId);
+  cudaDeviceProp props;
+  cudaGetDeviceProperties(&props, deviceId);
+    
+  int computeCapabilityMajor = props.major;
+  int computeCapabilityMinor = props.minor;
+  int multiProcessorCount    = props.multiProcessorCount;
+  int warpSize               = props.warpSize;
+  std::cout << "Device ID : " << deviceId << "\nNumber of SMs : " << multiProcessorCount 
+            << "\nCompute Capability Major : " << computeCapabilityMajor << "\nCompute Capability Minor : " 
+            << computeCapabilityMinor << "\n Warp Size : " << warpSize << std::endl;
+            
+  return 0;
+}
